7e245daf7dc7745a1aa5826241c332d4a870f0bf
Eddy Sag <Eddy.Saghour@etu.univ-savoie.fr>
2024-01-08 17:09:16 +0100

    sza

--- SAE1.01-1.02/MainWindow.xaml.cs
+++ SAE1.01-1.02/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SAE1._01_1._02
 {
@@ -20,26 +21,77 @@ namespace SAE1._01_1._02
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool haut, gauche,droite,bas = false;
+        private ImageBrush joueurSkin = new ImageBrush();
+        private ImageBrush sol1 = new ImageBrush();
+        private ImageBrush ennemi1 = new ImageBrush();
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        int compteur = 0, sprite = 1;
+
         public MainWindow()
         {
             InitializeComponent();
             WindowState = WindowState.Maximized;
+            dispatcherTimer.Tick += Jeu;
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(16);
+            //joueur1=joueurSkin
+            
         }
-        private void GameEngine(object sender, EventArgs e)
-        { 
-
-
-        }
-
-        private void DeplacementJoueur() 
-        { 
-        
+        private void Jeu(object sender, EventArgs e)
+        {
+            Rect joueur = new Rect(Canvas.GetLeft(joueur1), Canvas.GetTop(joueur1),
+            joueur1.Width, joueur1.Height);
 
         }
 
 
 
+        private void CanvasKeyIsDown(object sender, KeyEventArgs e)
+        {
+            // on gère les booléens gauche et droite en fonction de l’appui de la touche
+            if (e.Key == Key.Left)
+            {
+                gauche = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                droite = true;
+            }
+            if (e.Key == Key.Up) 
+            {
+                haut = true;
+            }
+            if (e.Key == Key.Down)
+            {
+                bas = true;
+            }
+        }
 
+        private void CanvasKeyIsUp(object sender, KeyEventArgs e)
+        {
+            // on gère les booléens gauche et droite en fonction du relâchement de la touche
+            if (e.Key == Key.Left)
+            {
+                gauche = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                droite = false;
+            }
+            if (e.Key == Key.Up)
+            {
+                haut = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                bas = false;
+            }
+        }
+        private void DeplacementJoueur()
+        {
+            if (gauche && Canvas.GetLeft(joueur1)>0)
+            { }
 
+        }
     }
-}
+ }
