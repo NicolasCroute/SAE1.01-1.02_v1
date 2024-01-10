e1d11c2cb50b480e02101428a36f88f6ee07b11e
Nicolas Croute <Nicolas.croute@etu.univ-smb.fr>
2024-01-10 17:29:58 +0100

    menu

--- SAE1.01-1.02/Menu.xaml.cs
+++ SAE1.01-1.02/Menu.xaml.cs
@@ -19,9 +19,47 @@ namespace SAE1._01_1._02
     /// </summary>
     public partial class Menu : Window
     {
+
+        private ImageBrush solFacileSkin = new ImageBrush();
+        private ImageBrush solDifficileSkin = new ImageBrush();
+        private bool modeJeu = false;
+        
+
         public Menu()
         {
             InitializeComponent();
+
+            WindowState = WindowState.Maximized;
+
+            solDifficileSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "images/sol/sol_Difficile.png"));
+            solFacileSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "images/sol/sol_facile.png"));
+            mode.Fill = solFacileSkin;
+
+
+        }
+
+        private void ChangeImage()
+        {
+            if (modeJeu == false)
+            {
+                
+                mode.Fill = solFacileSkin;
+            }
+            else if (modeJeu == true) 
+            {
+                
+                mode.Fill = solDifficileSkin;
+            }
+
+            //inverse l'état initiale des touche
+            modeJeu = !modeJeu;
+            
+        }
+
+        private void but_Mode_Click(object sender, RoutedEventArgs e)
+        {
+            
+            ChangeImage();
         }
     }
 }
